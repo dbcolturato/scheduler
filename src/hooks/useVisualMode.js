e24fead564963fba28e8a82bdb45dab870c69fdb
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-10-29 14:35:34 -0400

    Build Hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,26 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
+  function transition(newMode, replace = false) {
     setMode(newMode);
+    if (replace) {
+      const newHistory = history.slice();
+      newHistory[newHistory.length - 1] = newMode;
+      setHistory(newHistory);
+    } else {
+      setHistory(history.concat([newMode]));
+    }
   }
 
-  return { mode, transition };
+  function back() {
+    if (history.length > 1) {
+      const newHistory = history.slice(0, history.length - 1);
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1])
+    }
+  }
+
+  return { mode, transition, back };
 };
