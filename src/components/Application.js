f41afceba27e286702b35776a9027d477efb48e7
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-10-29 17:35:26 -0400

    Use custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -22,10 +22,8 @@ export default function Application(props) {
       axios.get("http://localhost:8001/api/appointments"),
       axios.get("http://localhost:8001/api/interviewers")
     ]).then((all) => {
-      console.log(all);
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviwers: all[2].data }))
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
     })
-
   }, [])
 
   const setDay = day => setState({ ...state, day });
