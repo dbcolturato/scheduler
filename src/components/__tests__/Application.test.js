da659877790c09ea40087aa96b2546e664597811
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-11-03 20:51:42 -0500

    Cancel a interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -18,7 +18,7 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container, debug } = render(<Application />);
-    
+
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
@@ -32,12 +32,38 @@ describe("Application", () => {
     fireEvent.click(getByText(appointment, "Save"));
     // debug();
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
-    
+
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+
+    const { container, debug } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(getByAltText(appointments, "Delete"));
+
+    expect(getByText(appointments, "Delete the appointment?").toBeInTheDocument);
+
+    fireEvent.click(getByText(appointments, "Confirm"));
+
+    expect(getByText(appointments, "Deleting")).toBeInTheDocument();
+
+    await waitForElement(() => getByAltText(appointments, "Add"));
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 });
